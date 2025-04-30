fd779ecf15bd23ee157a98caeed64ea2c4f0b41e
@ifdc2308 <italodorea123@gmail.com>
2025-04-30 11:51:06 -0300

    feat: wpp-btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 import { LandingPage } from "./pages/LandingPage/LandingPage";
 
 function App() {
-
   return (
     <>
+      <FloatingWhatsApp phoneNumber="557193852662" accountName="Open Soluções Tribuárias" />
       <LandingPage />
     </>
   );
