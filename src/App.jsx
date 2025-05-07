2441b325cc4b5b49bde2072c6c5b36aa484f97b4
@ifdc2308 <italodorea123@gmail.com>
2025-05-07 17:58:23 -0300

    fix-react-routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
 import { LandingPage } from "./pages/landingPage/LandingPage";
+import { AdminPage } from "./pages/admin/adminpage";
 
 function App() {
   return (
-    <>
-      <FloatingWhatsApp
-        phoneNumber="557193852662"
-        accountName="Open Soluções Tribuárias"
-        avatar="./icon-gtap.svg"
-        statusMessage="online"
-        chatMessage="Descreva sua solicitação para os nossos consultores de vendas"
-        placeholder="Descrever solicitação"
-      />
-      <LandingPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <>
+          <Route
+            path="/website-gtap"
+            element={
+              <>
+                <LandingPage />
+                <FloatingWhatsApp
+                  phoneNumber="557193852662"
+                  accountName="Open Soluções Tribuárias"
+                  avatar="./icon-gtap.svg"
+                  statusMessage="online"
+                  chatMessage="Descreva sua solicitação para os nossos consultores de vendas"
+                  placeholder="Descrever solicitação"
+                />
+              </>
+            }
+          />
+          <Route path="/website-gtap/admin" element={<AdminPage />} />
+        </>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/admin/AdminPage.jsx
@@ -0,0 +1,3 @@
+export const AdminPage = () => {
+  return <p>página admin</p>;
+};
