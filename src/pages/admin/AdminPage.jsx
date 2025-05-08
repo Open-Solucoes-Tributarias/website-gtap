6f42779429bbe3bbc9f64e0252d46905cbd63615
@ifdc2308 <italodorea123@gmail.com>
2025-05-08 11:08:19 -0300

    feat: login-page

--- src/pages/admin/AdminPage.jsx
+++ src/pages/admin/AdminPage.jsx
@@ -1,3 +1,24 @@
+import "./AdminPage.css";
+
 export const AdminPage = () => {
-  return <p>página admin</p>;
+  return (
+    <section className="login-page-section">
+      <div className="login-page-left-container">
+        <img src="./icon-gtap.svg" />
+        <p>Login de acesso Administrador</p>
+        <div className="form-input-admin">
+          <label htmlFor="email">Email *</label>
+          <input name="email" type="email" placeholder="Seu e-mail" required />
+        </div>
+        <div className="form-input-admin">
+          <label htmlFor="password">senha *</label>
+          <input name="password" type="password" placeholder="senha" required />
+        </div>
+        <div className="form-input-admin">
+          <button type="submit">ENTRAR</button>
+        </div>
+      </div>
+      <div className="login-page-right-container"></div>
+    </section>
+  );
 };
