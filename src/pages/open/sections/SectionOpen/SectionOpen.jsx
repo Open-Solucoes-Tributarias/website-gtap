6ef089692b11163300c98fe1c748ecb7503abc92
ifdc2308 <italodorea123@gmail.com>
2025-05-21 15:08:57 -0300

    feat-effect-digitacao

--- src/pages/open/sections/SectionOpen/SectionOpen.jsx
+++ src/pages/open/sections/SectionOpen/SectionOpen.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./SectionOpen.css"
 import globoOPen from "@/assets/globo-open.svg";
 
@@ -9,8 +10,45 @@ import image4 from "@/assets/open/image-apresentacao.jpg"
 import image5 from "@/assets/open/image-debate.jpg"
 import image6 from "@/assets/open/image-livro.png"
 
+const textsTitles = [
+    { id: 0, value: "Segurança Financeira" },
+    { id: 1, value: "Solidez Fiscal" },
+    { id: 1, value: "Estratégia Fiscal" },
+    { id: 1, value: "Conformidade" },
+    { id: 1, value: "Resultados" },
+    { id: 1, value: "Economia inteligente" },
+];
 
 export const SectionOpen = () => {
+
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [displayText, setDisplayText] = useState("&nbsp;");
+    const texts = textsTitles.map((t) => t.value);
+
+ useEffect(() => {
+    let charIndex = -1;
+    const fullText = texts[currentIndex];
+    setDisplayText("");
+
+    const typingInterval = setInterval(() => {
+        charIndex++;
+        if (charIndex < fullText.length) {
+            setDisplayText((prev) => prev + fullText[charIndex]);
+        } else {
+            clearInterval(typingInterval);
+            setTimeout(() => {
+                setCurrentIndex((prev) => (prev + 1) % texts.length);
+            }, 1500);
+        }
+    }, 100);
+
+    return () => clearInterval(typingInterval);
+}, [currentIndex]);
+
+
+
+
     return (
         <section className="section-sobre-open">
             <div className="section-left-open">
@@ -19,22 +57,22 @@ export const SectionOpen = () => {
                 </div>
                 <div className="src-page-open">
                     <p>
-                            Home&nbsp;&gt;&gt;&nbsp;<b>A Open</b>
+                        Home&nbsp;&gt;&gt;&nbsp;<b>A Open</b>
                     </p>
                 </div>
                 <div className="title-open-section">
                     <h2>Conhecimento
-                        tributário é <b>Segurança Financeira</b></h2>
+                        tributário é <br /><b className="typing-js">{displayText}</b></h2>
                 </div>
                 <div>
                     <button><img src={globoOPen} alt="globo open" />Saiba mais!</button>
                 </div>
                 <div className="stars-content-open">
-                    <i class="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
-                    <i class="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
-                    <i class="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
-                    <i class="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
-                    <i class="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
+                    <i className="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
+                    <i className="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
+                    <i className="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
+                    <i className="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
+                    <i className="fa-solid fa-star" style={{ color: "#dda92e" }}></i>
                 </div>
                 <div className="content-data-open">
                     <h3><b>+20 mil</b> alunos treinados!</h3>
